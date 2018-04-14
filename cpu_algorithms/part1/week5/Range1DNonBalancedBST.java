d0471982d833b3eadfd206e738a73448ff3adc92
awkonecki.developer@gmail.com <awkonecki.developer@gmail.com>
2018-04-13 17:19:28 -0700

    Empty non balanced BST 1D range implementation.

---
+++ cpu_algorithms/part1/week5/Range1DNonBalancedBST.java
@@ -0,0 +1,49 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Range1DNonBalancedBST <Key extends Comparable<Key>, Value> {
+
+    //*************************************************************************
+    //* PRIVATE FACING API
+    //************************************************************************* 
+    Node<Key, Value> mRoot = null;
+
+    private class Node <Key extends Comparable<Key>, Value> {
+
+    }
+
+    private Node<Key, Value> search(Node<Key, Value> root, Key key) {
+        return null;
+    }
+
+    //*************************************************************************
+    //* PUBLICLY FACING API
+    //************************************************************************* 
+    public int size() {
+        return 0;
+    }
+    
+    public boolean contains(Key key) {
+        return false;
+    }
+
+    public void insert(Key key, Value value) {
+
+    }
+
+    public int rank(Key key) {
+        return 0;
+    }
+
+    public int range_count(Key keyLow, Key keyHigh) {
+        return 0;
+    }
+
+    public List<Key> range_search(Key keyLow, Key keyHigh) {
+        return null;
+    }
+
+    public static void main(String [] args) {
+
+    }
+}
