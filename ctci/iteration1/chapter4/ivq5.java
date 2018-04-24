83d26ca4c61371a421f30aaa3889ed5a7bfa680f
awkonecki.developer@gmail.com <awkonecki.developer@gmail.com>
2018-04-24 14:23:27 -0700

    Chapter 4 Problem 5 Implementation Testing.

--- ctci/iteration1/chapter4/ivq5.java
+++ ctci/iteration1/chapter4/ivq5.java
@@ -1,5 +1,6 @@
 import java.util.List;
 import java.util.LinkedList;
+import java.util.Random;
 
 public class ivq5 <T extends Comparable <T>> {
     private Node<T> mRoot = null;
@@ -12,6 +13,60 @@ public class ivq5 <T extends Comparable <T>> {
         public Node(T value) {
             this.value = value;
         }
+    }
+
+    public void insert(T value) {
+        if (value == null) {
+            return;
+        }
+
+        this.mRoot = this.insert(this.mRoot, value);
+
+        return;
+    }
+
+    public void insertRandom(T value) {
+        if (value == null) {
+            return;
+        }
+
+        this.mRoot = this.insertRandom(this.mRoot, value);
+
+        return;
+    }
+
+    private Node<T> insert(Node<T> root, T value) {
+        if (root == null) {
+            return new Node<T>(value);
+        }
+
+        assert (root.value != null);
+
+        if (root.value.compareTo(value) > 0) {
+            root.left = this.insert(root.left, value);
+        }
+        else {
+            // Handling equal case.
+            root.right = this.insert(root.right, value);
+        }
+
+        return root;
+    }
+
+    private Node<T> insertRandom(Node<T> root, T value) {
+        if (root == null) {
+            return new Node<T>(value);
+        }
+
+        Random random = new Random();
+        if (random.ints(1, 0, 2).sum() == 0) {
+            root.left = this.insertRandom(root.left, value);
+        } 
+        else {
+            root.right = this.insertRandom(root.right, value);
+        }
+
+        return root;
     }
 
     public boolean isBSTArrayBase() {
@@ -93,5 +148,23 @@ public class ivq5 <T extends Comparable <T>> {
         }
 
         return value;
+    }
+
+    public static void main(String [] args) {
+        ivq5<Integer> treeOfIntegers = new ivq5<Integer>();
+
+        for (int index = 0; index < 100; index++) {
+            treeOfIntegers.insert(index);
+        }
+
+        assert (treeOfIntegers.isBST());
+        assert (treeOfIntegers.isBSTArrayBase());
+
+        for (int index = 0; index < 100; index++) {
+            treeOfIntegers.insertRandom(index);
+        }
+
+        assert (!treeOfIntegers.isBST());
+        assert (!treeOfIntegers.isBSTArrayBase());
     }
 }
