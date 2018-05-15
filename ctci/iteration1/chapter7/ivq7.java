e0b4e2d584fba5f742550e66928aacae5dca2aaf
awkonecki.developer@gmail.com <awkonecki.developer@gmail.com>
2018-05-14 22:07:53 -0700

    Chapter 7 Problem 7 Implementation.

---
+++ ctci/iteration1/chapter7/ivq7.java
@@ -0,0 +1,46 @@
+import java.lang.Math;
+
+public class ivq7 {
+    
+    public static long kthNumber(int k) {
+        if (k < 1) {
+            return 0;
+        }
+
+        int powerBase = (k - 1) / 3, power7 = 0, power5 = 0, power3 = 0;
+
+        switch(k % 3) {
+            case 1:
+                power3++;
+                break;
+            case 2:
+                power5++;
+                break;
+            case 0:
+                power7++;
+                break;
+            default:
+                break;
+        }
+
+        power3 += powerBase;
+        power5 += powerBase;
+        power7 += powerBase;
+
+        System.out.println(power3 + " " + power5 + " " + power7);
+
+        // The double back to integer rounding could cause issues
+        // Could do a base conversion perform essentially shift operations and then convert back
+        return Math.round(Math.pow(3, power3)) * Math.round(Math.pow(5, power5)) * Math.round(Math.pow(7, power7));
+    }
+    
+    public static void main(String [] args) {
+        assert (kthNumber(1) == 3);
+        assert (kthNumber(2) == 5);
+        assert (kthNumber(3) == 7);
+        assert (kthNumber(5) == (3 * 25 * 7));
+        // assert (kthNumber(6) == (3 * 5 * 49));
+        System.out.println(kthNumber(6));
+        assert (kthNumber(9) == (9 * 25 * 343));
+    }
+}
