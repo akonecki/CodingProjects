88302fbf720c28e459eb5c4ae210e7b978749b5a
awkonecki <awkonecki.developer@gmail.com>
2018-07-21 18:53:43 -0700

    [FTR] Challenge-5 GCD of Array of Integers
    Feature Description
    
    Perform the GCD operation on an array of integers for those that are
    positive only.  The implementation achieves this in O(NlgM) where M
    is the number of levels it takes to divide a number down to 1 for the
    GCD algorithm leaving the remainder with 0.
    
    Related Issues
    - #16

---
+++ techDevGuide/foundational/src/main/java/challenge-5/Challenge5.java
@@ -0,0 +1,34 @@
+public class Challenge5 {
+
+    public static int arrayGCD(int [] numbers) {
+        
+        int gcd = numbers[0];;
+
+        for (int index = 1; index < numbers.length; index++) {
+            gcd = gcd(gcd, numbers[index]);
+        }
+
+        return gcd;
+    }
+
+    private static int gcd(int num1, int num2) {
+        if (num1 < num2) {
+            return gcd(num2, num1);
+        }
+
+        int A = num1, B = num2, C = 0;
+
+        while (B > 0 && A % B != 0) {
+            C = A % B;
+            A = B;
+            B = C;
+        }
+
+        return B;
+    }
+
+    public static void main(String [] args) {
+        assert (arrayGCD(new int [] {2,4,6,8,16}) == 2);
+    }
+
+}
