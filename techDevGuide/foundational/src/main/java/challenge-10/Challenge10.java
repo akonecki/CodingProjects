2271c2c803e18e034342ded996997a9506f56f16
awkonecki <awkonecki.developer@gmail.com>
2018-07-23 22:12:03 -0700

    [FTR] Challenge-10 Recursive Count Analysis
    Challenge-10 [A]nalysis portion of the the recursive implementation.
    The analysis shows that it has suboptimal solution and has subproblems
    that reoccurr.
    
    Therefore this can be addressed by dynamic programming.  In addition,
    the runtime performance is exponential while the memory impact is linear
    or equal to the height of the tree.
    
    Related Issues
    - #23

--- techDevGuide/foundational/src/main/java/challenge-10/Challenge10.java
+++ techDevGuide/foundational/src/main/java/challenge-10/Challenge10.java
@@ -4,6 +4,86 @@ public class Challenge10 {
         return recursiveCount(nums, 0);
     }
 
+    // [A]nalysis of the recursive method
+    // 1 - Is the recursive solution suboptimal?
+    // Yes it is recursive by nature and only concerned about current step for
+    // calculating the total count.
+    //
+    // 2 - does it contain re-occurring values?
+    /*
+        nums[1,2,3,4], 0
+            nums[1,2,3,4], 1
+                nums[1,2,3,4], 2
+                    nums[1,2,3,4], 3
+                        nums[1,2,3,4], 4
+                            return 1
+                        nums[1,2,3,4], 4
+                            return 1
+                        return 2
+                    nums[1,2,3,4], 3
+                        nums[1,2,3,4], 4
+                            return 1
+                        nums[1,2,3,4], 4
+                            return 1
+                        return 2
+                    return 4
+                nums[1,2,3,4], 2
+                    nums[1,2,3,4], 3
+                        nums[1,2,3,4], 4
+                            return 1
+                        nums[1,2,3,4], 4
+                            return 1
+                        return 2
+                    nums[1,2,3,4], 3
+                        nums[1,2,3,4], 4
+                            return 1
+                        nums[1,2,3,4], 4
+                            return 1
+                        return 2
+                    return 4
+                return 8 
+            nums[1,2,3,4], 1
+                nums[1,2,3,4], 2
+                    nums[1,2,3,4], 3
+                        nums[1,2,3,4], 4
+                            return 1
+                        nums[1,2,3,4], 4
+                            return 1
+                        return 2
+                    nums[1,2,3,4], 3
+                        nums[1,2,3,4], 4
+                            return 1
+                        nums[1,2,3,4], 4
+                            return 1
+                        return 2
+                    return 4
+                nums[1,2,3,4], 2
+                    nums[1,2,3,4], 3
+                        nums[1,2,3,4], 4
+                            return 1
+                        nums[1,2,3,4], 4
+                            return 1
+                        return 2
+                    nums[1,2,3,4], 3
+                        nums[1,2,3,4], 4
+                            return 1
+                        nums[1,2,3,4], 4
+                            return 1
+                        return 2
+                    return 4
+                return 8 
+            return 16
+    */
+    // Yes - as from above I can see that there are multiple subproblems being
+    // re-computed multiple times
+    //
+    // TIME & SPACE COMPLEXITY
+    // Height of the tree = 4, which is N in this case
+    // Branching factor is set to (2) since 2 branches are taken at each level
+    // So total run time complexity is (2^4) or roughly (2^N) so exponential.
+    //
+    // The space complexity is that of the depth of the tree which is linear to
+    // the length of the array of (4) in this case and generally O(N).
     private static int recursiveCount(int [] nums, int index) {
         if (index == nums.length) {
             return 1;
