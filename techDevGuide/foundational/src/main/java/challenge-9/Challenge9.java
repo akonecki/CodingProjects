42a8bf9fb04d2ae5de0b710165721088c4973b2d
awkonecki <awkonecki.developer@gmail.com>
2018-07-23 21:51:19 -0700

    [FTR] Challenge-9 Fib
    Feature Description
    
    Challenge-9 Fib. notes for the [A]nalyze step of the problem with respect
    to the implemented recursive implementation done previously.
    
    As expected the implementation is expotential in run-time performance while
    the memory impact is the maximum height of the tree thus linear.
    
    Related Issues
    - #22

--- techDevGuide/foundational/src/main/java/challenge-9/Challenge9.java
+++ techDevGuide/foundational/src/main/java/challenge-9/Challenge9.java
@@ -1,5 +1,28 @@
 public class Challenge9 {
 
+    // Does the recursive implementation have optimal substructure?
+    // Yes - solving recursively into self contained subproblems.
+    // Does this have overlapping subproblems?
+    /*  
+        fib (4) 
+            fib (3)
+                fib (2)
+                    return 1
+                fib (1)
+                    return 1
+            fib (2)
+                return 1
+    */
+    // Yes - does have overlapping subproblems.  Multiple occurrences of
+    //       fib(2).
+    // TIME COMPLEXITY Calculation
+    //
+    // Height of tree is equal to (n - 1) due to return condition.
+    // Has 2 recusive calls so branching factor of (2)
+    // Number of calls is roughly 2^(4-1) or roughly 2^N
+    // Space complexity is O(N) due to the minimum height of the tree.
+    //
+    // So this is a perfect candidate for this problem.
     public static int recursiveFib(int n) {
         if (n == 1) {
             return 1;
