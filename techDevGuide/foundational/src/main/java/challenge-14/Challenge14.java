d5b72f3c3653bc75601508bac850c9934150d4f2
awkonecki <awkonecki.developer@gmail.com>
2018-07-26 13:35:53 -0700

    [FTR] Challenge-14 [A]nalysis : TargetSum
    Feature Description
    
    Analysis of the target sum recurision base functional implementation to
    determine if it is a candidate for dynammic programming.
    
    The current overall analysis is that is expontial based on the total number
    of elements in the list of numbers with the base equal to two (the branching
    factor).
    
    Related Issues
    - #29

--- techDevGuide/foundational/src/main/java/challenge-14/Challenge14.java
+++ techDevGuide/foundational/src/main/java/challenge-14/Challenge14.java
@@ -22,6 +22,24 @@ public class Challenge14 {
             targetSum(nums, target - nums[index], index + 1);
     }
 
+    // [A]nalysis
+    // [1] Is the implementation optimal substructure?
+    // Yes - the execution for each step of the recursion is self contained.
+    // [2] Does the problem have repeating subproblems?
+    // Yes - there are multiple calls onto the same index position with the
+    // same value is possible.
+
+    // Run Time Performance
+    // [1] The maximum height of the tree is equal to the total number of nums
+    // that are being investigated.
+    // [2] the Branching factor is (2) due to the fact that there are two possible
+    // operations per node at a given level.
+    // Run time performance is therefore 2^(N)
+
+    // Memory Performance
+    // Is equal to the height of the recursion tree due to manging the memory 
+    // associated with the call stack.  So O(N)
+
     public static void main (String [] args) {
         assert (targetSum(new int [] {1,2,3,4}, 0) == 2);
     }
