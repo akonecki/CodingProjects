7abb08570e5a420116cfc8b6dad83074fd8d0dc4
awkonecki <awkonecki.developer@gmail.com>
2018-07-28 18:48:29 -0700

    [FTR] Challenge-15 Initial [F]unctinoal Implementation
    Feature Description
    
    CHallenge-15 Edit Distance to determine the minimum number of operations
    for string equality  modification.  This first commit jsut sets up the
    initial public function api, while trying to determine what the recursive
    implementation should provide.
    
    Related Issues
    - #30

---
+++ techDevGuide/foundational/src/main/java/challenge-15/Challenge15.java
@@ -0,0 +1,19 @@
+public class Challenge15 {
+
+    // [F]unctional implementation
+    public static int operationCount(String s1, String s2) {
+        if (s1.length() > s2.length()) {
+            // Not supporting the delete operation only the insertion and 
+            // modification operation
+            return 0;
+        }
+
+
+    }
+
+    
+
+    public static void main(String [] args) {
+
+    }
+}
