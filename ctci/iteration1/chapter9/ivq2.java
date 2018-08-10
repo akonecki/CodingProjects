fd44ffe6f1b57c55d2dec7f8a389d01f57c13f2a
awkonecki <awkonecki.developer@gmail.com>
2018-08-09 20:11:00 -0700

    [FTR] Chapter 9 Problem 2 - [F]unctional Implementation
    Feature Description
    
    Recursive implementation that handles calculating the total number
    of unique paths there are from the origin to the designated cell.
    
    In this case the origin is 0,0 while the destination is the row,col
    of the provided matrix.
    
    The current implementation does not consider the occurrence of blocked
    locations.
    
    Related Issues

---
+++ ctci/iteration1/chapter9/ivq2.java
@@ -0,0 +1,25 @@
+public class ivq2 {
+
+    // [F]unctional Implementation
+    // Will first approach the case without any blocking cells within the matrix
+    public static int totalPaths(int [][] matrix) {
+        return totalPaths(matrix, 0, 0);
+    }
+
+    private static int totalPaths(int [][] matrix, int row, int col) {
+        if (row + 1 == matrix.length && col + 1 == matrix[row].length) {
+            return 1;
+        }
+        else if (row >= matrix.length || col >= matrix[row].length) {
+            return 0;
+        }
+
+        return totalPaths(matrix, row + 1, col) + totalPaths(matrix, row, col + 1);
+    }
+
+    public static void main(String [] args) {
+        int [][] matrix = new int [][] {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+
+        assert (totalPaths(matrix) == 6);
+    }
+}
