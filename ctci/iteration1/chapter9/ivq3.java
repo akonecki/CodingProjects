0602596ff4b0998c830604e4a15938b1d9fb700f
awkonecki <awkonecki.developer@gmail.com>
2018-08-11 15:44:21 -0700

    [FTR] Chapter 9 Problem 3 Recursive Implementation
    Feature Description
    
    Implementation of the recursive definition.  This is in contrast to the
    serial implementation which reduces the memory impact but the code is
    significantly more error prone.
    
    The memory impact due to the stack will be O(lgN) where N is the length
    of the input array.
    
    Related Issues

--- ctci/iteration1/chapter9/ivq3.java
+++ ctci/iteration1/chapter9/ivq3.java
@@ -66,6 +66,27 @@ public class ivq3 {
         return -1;
     }
 
+    public static boolean magicIndexExistsRecursive(int [] values) {
+        if (values == null || values.length == 0) {
+            return false;
+        }
+        return magicIndexExistsRecursive(values, 0, values.length - 1);
+    }
+
+    private static boolean magicIndexExistsRecursive(int [] values, int leftIndex, int rightIndex) {
+        if (leftIndex > rightIndex) {
+            return false;
+        }
+        
+        int index = (rightIndex - leftIndex) / 2 + leftIndex;
+
+        if (values[index] == index) {
+            return true;
+        }
+
+        return magicIndexExistsRecursive(values, leftIndex, index - 1) || magicIndexExistsRecursive(values, index + 1, rightIndex);
+    }
+
     public static void main(String [] args) {
         int [] negativeArray = {-1, -1, -1};
         int [] falseArray = {-9, -1, 0, 0, 1, 7, 19, 22, 23, 34};
@@ -74,12 +95,12 @@ public class ivq3 {
         int [] trueArrayDup = {-9, -1, 0, 0, 5, 5};
         int [] trueArrayDupPostOdd = {-9, -1, 0, 0, 1, 2, 2, 7, 7, 34};
         int [] trueArrayDupPreOdd = {-9, -1, 0, 0, 1, 2, 7, 7, 10, 34};
-        assert (!magicIndexExists(negativeArray));
-        assert (!magicIndexExists(falseArray));
-        assert (magicIndexExists(trueArrayStart));
-        assert (magicIndexExists(trueArrayEnd));
-        assert (magicIndexExists(trueArrayDup));
-        assert (magicIndexExists(trueArrayDupPostOdd));
-        assert (magicIndexExists(trueArrayDupPreOdd));
+        assert (magicIndexExists(negativeArray) == magicIndexExistsRecursive(negativeArray));
+        assert (magicIndexExists(falseArray) == magicIndexExistsRecursive(falseArray));
+        assert (magicIndexExists(trueArrayStart) == magicIndexExistsRecursive(trueArrayStart));
+        assert (magicIndexExists(trueArrayEnd) == magicIndexExistsRecursive(trueArrayEnd));
+        assert (magicIndexExists(trueArrayDup) == magicIndexExistsRecursive(trueArrayDup));
+        assert (magicIndexExists(trueArrayDupPostOdd) == magicIndexExistsRecursive(trueArrayDupPostOdd));
+        assert (magicIndexExists(trueArrayDupPreOdd) == magicIndexExistsRecursive(trueArrayDupPreOdd));
     }
 }
