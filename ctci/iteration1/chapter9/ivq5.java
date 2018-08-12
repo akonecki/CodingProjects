e0212d26003623915ca657a5b3fa917f3d47fc4f
awkonecki <awkonecki.developer@gmail.com>
2018-08-11 20:35:34 -0700

    [FTR] Chapter 9 Problem 5 - [A]nalysis
    Feature Description
    
    Analysis step of the problem.  Due to stateful elements existing within
    each recursive layer, the problem / recursive implementation is likely to
    not benefit from the traditional optiizations of dynamic programming in
    its current form.
    
    It is possible to implement a serial implementation to save on the
    recursive method overhead, however the permutation set will likely grow
    greater since it is not linear unlike the height of the problem.
    
    Related Issues

--- ctci/iteration1/chapter9/ivq5.java
+++ ctci/iteration1/chapter9/ivq5.java
@@ -48,6 +48,34 @@ public class ivq5 {
 
     }
 
+    // [A]nalysis
+    // 1. Does the problem have optimal substructure?
+    // Each level of the recursive function contributes its own state information
+    // which will make optimization from dynamic programming in the traditional
+    // context not as effective.
+    // 2. Does the problem have repeating subproblems
+    // Yes since there can be multiple instances of repeating strings going down
+    // to the lower indices.
+
+    // Run Time Analysis
+    // There are two possible branching factors
+    // 1. Include the character
+    // 2. Exclude the character
+
+    // The maximum depth of the recursion tree is based on the length of the 
+    // word itself.  However I have capped it such that the permutation string
+    // length is limited to 26, however this is not the worse case.
+
+    // Expected worse case run time performance expected will be on the order of
+    // O(2^(N)) where N is the length of the string.
+
+    // Run time Memory Impact
+    // The memory impact is quite large due to multiple stateful pieces of 
+    // information to ensure that only unique permutations of the characters
+    // are added to the result.
+
+    // Therefore the worse case is 2^N * 26
+
     public static void main(String [] args) {
         for (String string : getUniquePermutations("abaaabaa")) {
             System.out.println(string);
