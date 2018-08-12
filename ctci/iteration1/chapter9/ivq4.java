916724ded567e9c6842a0b42052dcd53b091df57
awkonecki <awkonecki.developer@gmail.com>
2018-08-11 19:28:50 -0700

    [FTR] Chapter 9 Problem 4 - [A]nalysis
    Feature Description
    
    Breakout of stating that the problem in its current form does not have
    optimal substructure due to the dynamic creation of a list in each prior
    step in order to maintain current state.  The overall time therefore will
    be worse than say just computing the total numnber of possible subsets.
    
    This does not mean the problem can not be implemented iteratively, however
    the memory impact will still be dominated by the O(2^N *N) for the set
    expanisions.
    
    Related Issues

--- ctci/iteration1/chapter9/ivq4.java
+++ ctci/iteration1/chapter9/ivq4.java
@@ -25,6 +25,29 @@ public class ivq4 {
         }
     }
 
+    // [A]nalysis
+    // 1. Does the problem contain optimal substructure
+    // The creation of the dynamic list within the step that does impact the 
+    // lower level so it would not necessarily be ideal in the current 
+    // implementation form
+    // 2. Does the problem contain repeating sub-problems
+    // Yes by revisiting sets it is seen that revisiting does occur even for the
+    // set population just from the output.
+
+    // Run time analysis
+    // Current form there are two branches of execution.
+    // 1. include
+    // 2. exclude
+
+    // Thus the branching factor is 2.
+
+    // The depth of the tree is the height of the array.
+
+    // The resulting worse case run time for execution is O(2^N).
+
+    // The run time memory impact is also 2^N * N, since 2^N is the number of 
+    // entries while N would be the max length.
+
     public static void main(String [] args) {
         int [] set = {1,2,3,4,5,6,7};
 
