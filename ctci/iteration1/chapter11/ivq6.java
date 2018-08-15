e6532689e0aba60d8589039cd26d01c3f191094b
awkonecki <awkonecki.developer@gmail.com>
2018-08-15 16:49:27 -0700

    [FTR] Chapter 11 Problem 6 - Less Strict Implementation
    Feature Description
    
    Implementation of the less data strict implementation.  Effectively provides
    a worse case operational time of O(M*lgN).
    
    Related Issues
    - #55

--- ctci/iteration1/chapter11/ivq6.java
+++ ctci/iteration1/chapter11/ivq6.java
@@ -14,6 +14,26 @@ public class ivq6 {
         }
     }
 
+    private static int binarySearch(int [] row, int value) {
+        int cMin = 0, cMax = row.length - 1;
+
+        while (cMin <= cMax) {
+            int index = (cMax - cMin) / 2 + cMin;
+
+            if (row[index] < value) {
+                cMin = index + 1;
+            }
+            else if (row[index] > value) {
+                cMax = index - 1;
+            }
+            else {
+                return index;
+            }
+        }   
+
+        return -1;
+    }
+
     public static Position findElementStrict(int [][] matrix, int value) {
         int rMin = 0, rMax = matrix.length - 1;
 
@@ -27,23 +47,7 @@ public class ivq6 {
                 rMin = rIndex + 1;
             }
             else {
-                int cMin = 0, cMax = matrix[rIndex].length - 1;
-                int cIndex = -1;
-
-                while (cMin <= cMax) {
-                    int index = (cMax - cMin) / 2 + cMin;
-
-                    if (matrix[rIndex][index] < value) {
-                        cMin = index + 1;
-                    }
-                    else if (matrix[rIndex][index] > value) {
-                        cMax = index - 1;
-                    }
-                    else {
-                        cIndex = index;
-                        break;
-                    }
-                }
+                int cIndex = binarySearch(matrix[rIndex], value);
 
                 if (cIndex != -1) {
                     return new Position(rIndex, cIndex);
@@ -57,8 +61,26 @@ public class ivq6 {
         return null;
     }
     
+    public static Position findElement(int [][] matrix, int value) {
+        int rIndex = 0;
+
+        for (int [] row : matrix) {
+            if (row[0] <= value && row[row.length - 1] >= value) {
+                int index= binarySearch(row, value);
+
+                if (index != -1) {
+                    return new Position(rIndex, index);
+                }
+            }
+            rIndex++;
+        }
+
+        return null;
+    }
+
     public static void main(String [] args) {
         assert (findElementStrict(new int [][] {{1,2,3,4}, {5,6,7,8}, {9,10,11,12}}, 6) != null);
         assert (findElementStrict(new int [][] {{1,2,3,4}, {5,6,7,8}, {9,10,11,12}}, 6).equals(new Position(1, 1)));
+        assert (findElementStrict(new int [][] {{1,2,3,4}, {5,6,7,8}, {9,10,11,12}}, 6).equals(findElement(new int [][] {{1,2,3,4}, {5,6,7,8}, {9,10,11,12}}, 6)));
     }
 }
