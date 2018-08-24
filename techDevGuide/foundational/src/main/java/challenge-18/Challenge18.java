aa42f142e1e6c51832cc92eb704b1b9edf6880e3
awkonecki <awkonecki.developer@gmail.com>
2018-08-23 21:58:58 -0700

    [FTR] Challenge 18 - Stack Based Solution for Max Binary Tree
    Feature Description
    
    The serial implementation only iterates through the array of nums once by
    using a stack.  The stack will contain the largest number at the bottom
    in the form of the node, and each root node value on top must be smaller
    and come after the one below it in the array.
    
    If the new node that is created is larger than the top of the stack pop
    it off into a temporary tree.  Keep poping off as long as the new node value
    is greater than the current top of the stack.  As nodes are popped off
    they will act as the parent to the previous node that was previous popped off
    with the child being on the right.  Once a value that is equal or greater
    or the stack is empty, the place the last popped off node as the new node's
    left and push the new node onto the stack.
    
    At the end empty the stack with each popped off being the child of the next
    layer but to the right.
    
    Another important aspect is that the order of the nodes in an inorder
    traversal shall result in traversal of values that is equal to the order
    of the values in the array.
    
    Related Issues
    - #75

--- techDevGuide/foundational/src/main/java/challenge-18/Challenge18.java
+++ techDevGuide/foundational/src/main/java/challenge-18/Challenge18.java
@@ -20,7 +20,45 @@ public class Challenge18 {
             return null;
         } 
 
-        return buildMaxTree(nums, 0, nums.length - 1);
+        Stack<Node> stack = new Stack<Node>();
+
+        int index = 0;
+        for (int num : nums) {
+            Node newNode = new Node(num, index);
+
+            if (stack.isEmpty()) {
+                stack.push(newNode);
+            }
+            else if (stack.peek().value >= newNode.value) {
+                stack.push(newNode);
+            }
+            else {
+                Node prev = null;
+
+                while (!stack.isEmpty() && stack.peek().value < newNode.value) {
+                    if (prev == null) {
+                        prev = stack.pop();
+                    }
+                    else {
+                        Node temp = stack.pop();
+                        temp.right = prev;
+                        prev = temp;
+                    }
+                }
+
+                newNode.left = prev;
+                stack.push(newNode);
+            }
+        }
+
+        Node prev = !stack.isEmpty() ? stack.pop() : null;
+        while (!stack.isEmpty()) {
+            Node temp = stack.pop();
+            temp.right = prev;
+            prev = temp;
+        }
+
+        return prev;
     }
 
     private static Node buildMaxTree(int [] nums, int lowIndex, int highIndex) {
@@ -67,9 +105,40 @@ public class Challenge18 {
         return maxIndex;
     }
 
+    public static ArrayList<Integer> inorderTraversal(Node root) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        inorderTraversal(root, result);
+        return result;
+    }
+
+    private static void inorderTraversal(Node root, ArrayList<Integer> result) {
+        if (root == null) {
+            return;
+        }
+        else {
+            inorderTraversal(root.left, result);
+            result.add(root.value);
+            inorderTraversal(root.right, result);
+        }
+    }
+
+    public static boolean isEqual(ArrayList<Integer> nums1, int [] nums2) {
+        if (nums1.size() != nums2.length) {
+            return false;
+        }
+        
+        for (int index = 0; index < nums1.size(); index++) {
+            if (nums1.get(index).intValue() != nums2[index]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     public static void main(String [] args) {
         int [] nums = {1, 6, 8, 9, 7, 6, 4, 3, 2, 1, 7, 7, 7, 3, 1, 0, 9};
 
-        buildMaxTree(nums);
+        assert (isEqual(inorderTraversal(buildMaxTree(nums)), nums));
     }
 }
