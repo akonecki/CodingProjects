c0162101b965ab0e73705049005101de28cc2488
awkonecki <awkonecki.developer@gmail.com>
2018-09-17 20:29:44 -0700

    [FTR] Challenge 32 - BST Inorder Iterator Implementation
    Feature Description
    
    Population of the `next`, `hasNext` and `remove` functions within the
    iterator class.
    
    Related Issues
    - #127

--- techDevGuide/foundational/src/main/java/challenge-32/Solution.java
+++ techDevGuide/foundational/src/main/java/challenge-32/Solution.java
@@ -1,4 +1,5 @@
 import java.util.Iterator;
+import java.util.Stack;
 
 public class Solution {
 
@@ -12,26 +13,57 @@ public class Solution {
         }
 
         public Iterator<Node> iterator() {
-            return new InorderTraversal();
+            return new InorderTraversal(this);
         }
 
         private class InorderTraversal implements Iterator<Node> {
+            private Stack<Node> nodes = new Stack<Node>();
+
+            public InorderTraversal(Node root) {
+                Node node = root;
+
+                // only pushing on the left most branch
+                while (node != null) {
+                    nodes.push(node);
+                    node = node.left;
+                }
+            }
+            
             public boolean hasNext() {
-                return false;
+                return !nodes.isEmpty();
             }
 
             public Node next() {
-                return null;
+                Node node = null;
+                Node traversalNode = null;
+
+                if (nodes.isEmpty()) {
+                    return node;
+                }
+
+                node = nodes.pop();
+                traversalNode = node.right;
+
+                while (traversalNode != null) {
+                    nodes.push(traversalNode);
+                    traversalNode = traversalNode.left;
+                }
+
+                return node;
             }
 
             public void remove() {
-
+                throw new java.lang.UnsupportedOperationException("Remove not supported.");
             }
         }
     } 
 
     public static void main(String [] args) {
+        Node root = new Node(1);
 
+        for (Node node : root) {
+            System.out.println(node.value);
+        }
 
     }
 }
