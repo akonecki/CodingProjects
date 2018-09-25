6c9b8c2a1091c0b16c58d23bbe48737318f8a19d
awkonecki <awkonecki.developer@gmail.com>
2018-09-24 17:20:36 -0700

    [FTR] STL List Prototyping
    Feature Description
    
    Working with the basic list type which as defined in the docs is a
    doubly linked list implementation.  Can use some of the same templated
    functions as vector but not operations are supported (i.e. indexing [],
    and `.at(index)`).
    
    For specific on insertion need to use iterator that indicates where the
    insertion is going to happen.  Insert does not perform the traversal only
    the modification of the node pointers.
    
    Related Issues
    - #161

--- prototype/src/cpp/stl/stl.cpp
+++ prototype/src/cpp/stl/stl.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 #include <vector>
+#include <list>
 
 using namespace std;
 
-void printVector(vector<int> data) {
+template <typename T> void printVector(T data) {
     cout << data.size() << endl;
     for (auto i = data.begin(); i != data.end(); i++) {
         cout << *i << " ";
     }
     cout << endl;
 
+    // These only work for contiguous memory containers.
+    /*
     for (int i = 0; i < data.size(); i++) {
         cout << data[i] << " ";
     }
@@ -19,6 +22,7 @@ void printVector(vector<int> data) {
         cout << data.at(i) << " ";
     }
     cout << endl;
+    */
 }
 
 int foo(int i) {
@@ -38,6 +42,7 @@ void swapVector(vector<int>  *data) {
 int main(int argc, char * argv []) {
     
     vector<int> data;
+    list<int> myList;
 
     for (int i = 0; i < 10; i++) {
         data.push_back(i);
@@ -56,5 +61,11 @@ int main(int argc, char * argv []) {
     swapVector(&data);
     printVector(data);
 
+    myList.push_back(1);
+    myList.push_front(2);
+    auto it = myList.begin();
+    it++;
+    myList.insert(it, 3);
+    printVector(myList);
     return 0;
 }
