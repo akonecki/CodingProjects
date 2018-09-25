916cf1edabb6f33d41eb8660e5a2051a9a88e72a
awkonecki <awkonecki.developer@gmail.com>
2018-09-25 14:20:14 -0700

    [FTR] C++ STL Container Adapters - Priority Queue
    Feature Description
    
    There is a default used comparator function for any created priority
    queue.  Essentailly tries to supply a max queue for the given data
    type.  If want to change this must supply a class that overloads the
    `operator()` with the desired behavior.
    
    Related Issues
    - #165

--- prototype/src/cpp/stl/stl_container_adapters.cpp
+++ prototype/src/cpp/stl/stl_container_adapters.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <queue>
+#include <vector>
 
 using namespace std;
 
@@ -15,16 +16,43 @@ void print(T data) {
     cout << endl;
 }
 
+template <typename T>
+void printTop(T data) {
+    // Queue by default does not by default support iteration.  Significantly 
+    // more restricted in access.
+    while (!data.empty()) {
+        cout << data.top() << " ";
+        data.pop();
+    }
+    cout << endl;
+}
+
+class MyComparison {
+    public:
+        bool operator() (const int & num1, const int & num2) {
+            if (num1 < num2) {
+                return false;
+            }
+            return true;
+        }
+};
+
 int main(int argc, char * argv[]) {
     queue<int> queue;
+    priority_queue<int> pqueue;
+    int values [] = {15, 38, 1, 14, 76, 32};
 
     for (int i = 0; i < 10; i++) {
         queue.push(i);
+        pqueue.push(i);
     }
 
     print(queue);
     cout << queue.size() << endl;
     print(queue);
-    
+    printTop(pqueue);
+
+    priority_queue<int, vector<int>, MyComparison> comparePQueue (values, values+4);
+    printTop(comparePQueue);
     return 0;
 }
