a96260d0ee6b8e35bc6922ad2f5c1a4f0fce7523
awkonecki <awkonecki.developer@gmail.com>
2018-09-25 15:00:21 -0700

    [FTR] C++ STL Associative Containers - Map
    Feature Description
    
    Works on the `pair` structure for the insertion and iteration of
    elements from the data structure.
    
    MultiMap allows for duplicate keys, and is once again all ordered.
    
    Related Issues
    - #167

--- prototype/src/cpp/stl/stl_associative_containers.cpp
+++ prototype/src/cpp/stl/stl_associative_containers.cpp
@@ -1,6 +1,7 @@
 #include <cstdlib>
 #include <iostream>
 #include <set>
+#include <map>
 
 using namespace std;
 
@@ -32,14 +33,17 @@ int main(int argc, char * argv[]) {
     int values [] = {1, 15, 29, 13, 11, 7, 9, 11, -1};
     multiset<int> myset (values, values+8);
     set<int, MyComparable> compSet;
+    map<int, int> myMap;
 
     for (int i = 0; i < 10; i++) {
         myset.insert(i);
         compSet.insert(i);
+        myMap.insert(pair<int, int> (i, i * -1));
     }
 
     print(myset);
     print(compSet);
+    // print(myMap);
 
     return 0;
 }
