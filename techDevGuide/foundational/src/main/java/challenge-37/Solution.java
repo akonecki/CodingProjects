5c467a14e176fdc7538215ff8e48552768f4197d
awkonecki <awkonecki.developer@gmail.com>
2018-09-29 22:13:15 -0700

    [FTR] Challenge 37 - Island Count Implementation
    Feature Description
    
    Serial implementation of the counting of the number of islands in the
    given map.  The does require extra memory (implementation with copy of
    number of rows).
    
    The implementation does restrict performance to that of only being equal
    to a O(N*M) for the traversal of the map itself.
    
    Related Issues
    - #179

--- techDevGuide/foundational/src/main/java/challenge-37/Solution.java
+++ techDevGuide/foundational/src/main/java/challenge-37/Solution.java
@@ -1,3 +1,75 @@
+import java.util.HashSet;
+
 public class Solution {
-    
+    public static int numberOfIslands(int [][] map) {
+        int [] dp = new int [map[0].length];
+        HashSet<Integer> uniqueIslandIds = new HashSet<Integer>();
+        int idCount = 0;
+
+        for (int row = 0; row < map.length; row++) {
+            for (int col = 0; col < map[row].length; col++) {
+                if (map[row][col] != 0) {
+                    int left = 0, up = 0, min = 0;
+
+                    if (row - 1 >= 0 && dp[col] != 0) {
+                        up = dp[col];
+                    }
+
+                    if (col - 1 >= 0 && dp[col - 1] != 0) {
+                        left = dp[col - 1];
+                    }
+
+                    if (left != 0 && up != 0) {
+                        if (left != up) {
+                            uniqueIslandIds.remove(Math.max(left, up));
+                        }
+                        // merge to min value of the two
+                        dp[col] = Math.min(left, up);
+                    }
+                    else if (left != 0) {
+                        dp[col] = left;
+                    }
+                    else if (up != 0) {
+                        dp[col] = up;
+                    }
+                    else {
+                        // new
+                        idCount++;
+                        dp[col] = idCount;
+                        uniqueIslandIds.add(idCount);
+                    }
+                }
+                else {
+                    dp[col] = 0;
+                }
+                System.out.print(dp[col] + " ");
+            }
+            System.out.println("");
+        }
+        System.out.println(uniqueIslandIds.size());
+        return uniqueIslandIds.size();
+    }
+
+    public static void main(String [] args) {
+        assert (numberOfIslands(new int [][] {
+            {1, 1, 1, 1, 1},
+            {1, 1, 1, 1, 1}
+        }) == 1);
+        assert (numberOfIslands(new int [][] {
+            {1, 0, 1, 0, 1},
+            {0, 1, 0, 1, 0}
+        }) == 5);
+        assert (numberOfIslands(new int [][] {
+            {1, 0, 1, 1, 1},
+            {1, 1, 0, 1, 0},
+            {1, 0, 1, 0, 1},
+            {0, 1, 1, 1, 0}
+        }) == 4);
+        assert (numberOfIslands(new int [][] {
+            {1, 1, 1, 1, 1},
+            {1, 0, 1, 0, 1},
+            {1, 0, 1, 0, 1},
+            {1, 1, 1, 1, 1}
+        }) == 1);
+    }
 }
