39b1a7a7e3003226bd3eba9bb24043cb05119dbd
awkonecki <awkonecki.developer@gmail.com>
2018-09-30 12:18:11 -0700

    [FTR] Challenge 38 - Water Volume Stack Implementation
    Feature Description
    
    Stack based implementation that is responsible for calculating the total volume
    based on the array of heights.  The key here is handling the empty list after
    the pop of prev height, and pushing on the correct index after the fact which
    indicates the current water level that has been accounted for.
    
    Related Issues
    - #180

--- techDevGuide/foundational/src/main/java/challenge-38/Solution.java
+++ techDevGuide/foundational/src/main/java/challenge-38/Solution.java
@@ -1,116 +1,54 @@
-import java.util.HashMap;
+import java.util.Stack;
 import java.util.Random;
-import java.util.List;
-import java.util.LinkedList;
 
 public class Solution {
-    public static boolean numberOfIslands(int [][] map) {
-        int serialResult = numberOfIslandsSerial(map);
-        int recursiveResult = numberOfIslandsRecursive(map);
-        assert (serialResult == recursiveResult);
-        return true;
-    }
     
-    private static int numberOfIslandsRecursive(int [][] map) {
-        int count = 0;
-        for (int row = 0; row < map.length; row++) {
-            for (int col = 0; col < map[row].length; col++) {
-                if (map[row][col] != -1 & map[row][col] != 0) {
-                    count++;
-                    numberOfIslands(map, row, col);
+    public static int waterVolume(int [] heights) {
+
+    }
+
+    private static int waterVolumeScan(int [] heights) {
+
+    }
+
+    private static int waterVolumeStack(int [] heights) {
+        Stack<Integer> heightStack = new Stack<Integer>();
+        Stack<Integer> indexStack = new Stack<Integer>();
+        int volume = 0;
+
+        for (int index = 0; index < heights.length; index++) {
+            int height = heights[index];
+
+            if (heightStack.isEmpty() || heightStack.peek().intValue() >= height) {
+                // just push onto the stack
+                heightStack.push(height);
+                indexStack.push(index);
+            }
+            else {
+                int prevHeight = height;
+                int prevIndex = index;
+
+                while (!heightStack.isEmpty() && heightStack.peek().intValue() < height) {
+                    prevHeight = heightStack.pop().intValue();
+                    prevIndex = indexStack.pop().intValue();
+
+                    if (heightStack.isEmpty()) {
+                        break;
+                    }
+
+                    volume += Math.abs(prevHeight - Math.min(height, heightStack.peek().intValue())) * (index - prevIndex);
                 }
+
+                heightStack.push(height);
+                indexStack.push(prevIndex);
             }
         }
-        System.out.println("Recursive Size " + count);
-        return count;
-    }
 
-    private static void numberOfIslands(int [][] map, int row, int col) {
-        if (row >= map.length || row < 0 || col < 0 || col >= map[row].length || map[row][col] == -1 || map[row][col] == 0) {
-            return;
-        }
-
-        map[row][col] = -1;
-        numberOfIslands(map, row, col - 1);
-        numberOfIslands(map, row, col + 1);
-        numberOfIslands(map, row - 1, col);
-        numberOfIslands(map, row + 1, col);
-    }
-
-    private static int getParent(HashMap<Integer, Integer> mappingIds, int key) {
-        while (mappingIds.get(key).intValue() != key) {
-            key = mappingIds.get(key).intValue();
-        }
-
-        return key;
-    }
-
-    private static int numberOfIslandsSerial(int [][] map) {
-        int [] dp = new int [map[0].length];
-        // Key - now represents a precursor in the graph
-        HashMap<Integer, Integer> mappingIds = new HashMap<>();
-        int idCount = 0;
-        int uniqueCount = 0;
-
-        for (int row = 0; row < map.length; row++) {
-            for (int col = 0; col < map[row].length; col++) {
-                if (map[row][col] != 0) {
-                    int left = 0, up = 0;
-
-                    if (row - 1 >= 0 && dp[col] != 0) {
-                        up = dp[col];
-                    }
-
-                    if (col - 1 >= 0 && dp[col - 1] != 0) {
-                        left = dp[col - 1];
-                    }
-
-                    if (left != 0 && up != 0) {
-                        // would have to setup a set & getRoot function to use this.
-                        int leftParent = getParent(mappingIds, left);
-                        int upParent = getParent(mappingIds, up);
-
-                        if (leftParent == upParent) {
-                            dp[col] = leftParent;
-                        }
-                        else {
-                            if ((new Random()).nextBoolean()) {
-                                dp[col] = leftParent;
-                                mappingIds.put(upParent, leftParent);
-                            }
-                            else {
-                                dp[col] = upParent;
-                                mappingIds.put(leftParent, upParent);
-                            }
-                            uniqueCount--;
-                        }
-                    }
-                    else if (left != 0) {
-                        dp[col] = left;
-                    }
-                    else if (up != 0) {
-                        dp[col] = up;
-                    }
-                    else {
-                        // new root within the set
-                        idCount++;
-                        dp[col] = idCount;
-                        mappingIds.put(idCount, idCount);
-                        uniqueCount++;
-                    }
-                }
-                else {
-                    dp[col] = 0;
-                }
-                System.out.print(dp[col] + " ");
-            }
-            System.out.println("");
-        }
-        System.out.println("Serial Size " + uniqueCount);
-        return uniqueCount;
+        return volume;
     }
 
     public static void main(String [] args) {
+        /*
         Random random = new Random();
         final int count = 1000;
 
@@ -130,6 +68,6 @@ public class Solution {
             System.out.println("");
             numberOfIslands(map);
         }
-        
+        */
     }
 }
