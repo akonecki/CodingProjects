eeec5f3ae75a1c928f31c64b703bfa9ef6cef951
awkonecki <awkonecki.developer@gmail.com>
2018-09-29 22:37:39 -0700

    [FTR] Challenge 37 - Recursive Implementation
    Feature Description
    
    Providing of recursive implementation to provide an testing base for correctness
    since it is easier to implement correctly.
    
    Additionally provided some generation of random data sets to test both implementations
    and will now error if there is a difference between the two.
    
    Current there is a bug to to the reduction of unique value not occurring just yet.
    
    Related Issues
    - #179

--- techDevGuide/foundational/src/main/java/challenge-37/Solution.java
+++ techDevGuide/foundational/src/main/java/challenge-37/Solution.java
@@ -1,7 +1,41 @@
 import java.util.HashSet;
+import java.util.Random;
 
 public class Solution {
-    public static int numberOfIslands(int [][] map) {
+    public static boolean numberOfIslands(int [][] map) {
+        int serialResult = numberOfIslandsSerial(map);
+        int recursiveResult = numberOfIslandsRecursive(map);
+        assert (serialResult == recursiveResult);
+        return true;
+    }
+    
+    private static int numberOfIslandsRecursive(int [][] map) {
+        int count = 0;
+        for (int row = 0; row < map.length; row++) {
+            for (int col = 0; col < map[row].length; col++) {
+                if (map[row][col] != -1 & map[row][col] != 0) {
+                    count++;
+                    numberOfIslands(map, row, col);
+                }
+            }
+        }
+        System.out.println("Recursive Size " + count);
+        return count;
+    }
+
+    private static void numberOfIslands(int [][] map, int row, int col) {
+        if (row >= map.length || row < 0 || col < 0 || col >= map[row].length || map[row][col] == -1 || map[row][col] == 0) {
+            return;
+        }
+
+        map[row][col] = -1;
+        numberOfIslands(map, row, col - 1);
+        numberOfIslands(map, row, col + 1);
+        numberOfIslands(map, row - 1, col);
+        numberOfIslands(map, row + 1, col);
+    }
+
+    private static int numberOfIslandsSerial(int [][] map) {
         int [] dp = new int [map[0].length];
         HashSet<Integer> uniqueIslandIds = new HashSet<Integer>();
         int idCount = 0;
@@ -46,30 +80,30 @@ public class Solution {
             }
             System.out.println("");
         }
-        System.out.println(uniqueIslandIds.size());
+        System.out.println("Serial Size " + uniqueIslandIds.size());
         return uniqueIslandIds.size();
     }
 
     public static void main(String [] args) {
-        assert (numberOfIslands(new int [][] {
-            {1, 1, 1, 1, 1},
-            {1, 1, 1, 1, 1}
-        }) == 1);
-        assert (numberOfIslands(new int [][] {
-            {1, 0, 1, 0, 1},
-            {0, 1, 0, 1, 0}
-        }) == 5);
-        assert (numberOfIslands(new int [][] {
-            {1, 0, 1, 1, 1},
-            {1, 1, 0, 1, 0},
-            {1, 0, 1, 0, 1},
-            {0, 1, 1, 1, 0}
-        }) == 4);
-        assert (numberOfIslands(new int [][] {
-            {1, 1, 1, 1, 1},
-            {1, 0, 1, 0, 1},
-            {1, 0, 1, 0, 1},
-            {1, 1, 1, 1, 1}
-        }) == 1);
+        Random random = new Random();
+        final int count = 1000;
+
+        for (int i = 0; i < 1000; i++) {
+            int rows = random.nextInt(9) + 1;
+            int cols = random.nextInt(9) + 1;
+
+            int [][] map = new int [rows][cols];
+            System.out.println("New Map:");
+            for (int row = 0; row < rows; row++) {
+                for (int col = 0; col < cols; col++) {
+                    map[row][col] = random.nextBoolean() ? 1 : 0;
+                    System.out.print(map[row][col] + " ");
+                }
+                System.out.println("");
+            }
+            System.out.println("");
+            numberOfIslands(map);
+        }
+        
     }
 }
