b14b5fbcb3590e42be597dab948a71de83a17483
awkonecki <awkonecki.developer@gmail.com>
2018-10-01 12:57:35 -0700

    [FTR] Chapter 8 Problem 1 - [A]nalysis Triple Step
    Feature Description
    
    Providing the basic set of analysis and determining if this problem is a candidate for
    being a dynamic programming problem.
    
    Related Issues
    - #183

--- ctci/iteration2/chapter8/src/java/problem1/Solution.java
+++ ctci/iteration2/chapter8/src/java/problem1/Solution.java
@@ -16,7 +16,32 @@ public class Solution {
         return waysToGo(N, step + 1) + waysToGo(N, step + 2) + waysToGo(N, step + 3);
     }
 
+    // [A]nalysis 
+    // 1 - Does the problem contain optimal substructure.
+    // Yes - each step is self contained.
+    // 2 - Does the problem have re-occurring calls.
+    // Yes - there will be multiple times in which the later step values are
+    // called multiple times.
+
+    // Space Complexity
+    // Stack space is purely impacted the most. 
+    // Worse case will be the height of the call tree, which would be for the 
+    // step 1 increments leading to a stack height of N in the worse case where
+    // N is a positive number indicating the total number of steps to go up.
+
+    // O(N)
+
+    // Time Complexity
+    // 3 branching factors
+    // max height is N, where N is the number of steps in the problem.
+    
+    // O(3^N)
+
     private static int waysToGoSerial(int N) {
+
+    }
+
+    public static void main(String [] args) {
 
     }
 }
