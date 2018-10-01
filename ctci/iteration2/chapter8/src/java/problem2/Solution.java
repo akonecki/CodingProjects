2e4c2dc36e293ce294bde44e27dbe1f1d2430205
awkonecki <awkonecki.developer@gmail.com>
2018-10-01 15:21:20 -0700

    [FTR] Chapter 8 Problem 2 - [T]urn Problem Around Robot in Grid
    Feature Description
    
    Implemented the serial implementation with dynamic programming for obtaining
    the total number of paths since it is the only one that really fits both sets
    of criteria for dp problems.
    
    Related Issues
    - #183

--- ctci/iteration2/chapter8/src/java/problem2/Solution.java
+++ ctci/iteration2/chapter8/src/java/problem2/Solution.java
@@ -1,5 +1,6 @@
 import java.util.List;
 import java.util.LinkedList;
+import java.util.Random;
 
 public class Solution {
     // [F]unctional
@@ -11,14 +12,10 @@ public class Solution {
     // a value of 0 indicates that the cell is open.
     // a value of 1 indicates that the cell is occupied.
     private static int totalPaths(int [][] matrix, int row, int col) {
-        if (row > matrix.length || (row != matrix.length && col > matrix[col].length)) {
-            return 0;
-        }
-        else if (row == matrix.length && col == matrix[row - 1].length) {
-            // at the bottom right cell of the matrix.
+        if (row == matrix.length - 1 && col == matrix[row].length - 1 && matrix[row][col] != 1) {
             return 1;
         }
-        else if (row == matrix.length || col == matrix[row - 1].length || matrix[row][col] == 1) {
+        else if (row >= matrix.length || col >= matrix[row].length || matrix[row][col] == 1) {
             return 0;
         }
 
@@ -26,15 +23,11 @@ public class Solution {
     }
 
     private static void validPaths(int [][] matrix, int row, int col, List<List<Integer>> path, List<List<List<Integer>>> paths) {
-        if (row > matrix.length || (row != matrix.length && col > matrix[col].length)) {
-            return;
-        }
-        else if (row == matrix.length && col == matrix[row - 1].length) {
-            // at the bottom right cell of the matrix.
+        if (row == matrix.length - 1 && col == matrix[row].length - 1 && matrix[row][col] != 1) {
             paths.add(new LinkedList<List<Integer>>(path));
             return;
         }
-        else if (row == matrix.length || col == matrix[row - 1].length || matrix[row][col] == 1) {
+        else if (row >= matrix.length || col >= matrix[row].length || matrix[row][col] == 1) {
             return;
         }
         
@@ -54,16 +47,12 @@ public class Solution {
     }
 
     private static boolean validPath(int [][] matrix, int row, int col, List<List<Integer>> path) {
-        if (row > matrix.length || (row != matrix.length && col > matrix[col].length)) {
-            return false;
-        }
-        else if (row == matrix.length && col == matrix[row - 1].length) {
-            // at the bottom right cell of the matrix.
+        if (row == matrix.length - 1 && col == matrix[row].length - 1 && matrix[row][col] != 1) {
             return true;
         }
-        else if (row == matrix.length || col == matrix[row - 1].length || matrix[row][col] == 1) {
+        else if (row >= matrix.length || col >= matrix[row].length || matrix[row][col] == 1) {
             return false;
-        }   
+        } 
 
         List<Integer> newPoint = new LinkedList<Integer>();
         newPoint.add(row);
@@ -113,14 +102,10 @@ public class Solution {
 
     // [S]ubproblem Identification & Momeization
     private static int totalPaths(int [][] matrix, int row, int col, int [][] dp) {
-        if (row > matrix.length || (row != matrix.length && col > matrix[col].length)) {
-            return 0;
-        }
-        else if (row == matrix.length && col == matrix[row - 1].length) {
-            // at the bottom right cell of the matrix.
+        if (row == matrix.length - 1 && col == matrix[row].length - 1 && matrix[row][col] != 1) {
             return 1;
         }
-        else if (row == matrix.length || col == matrix[row - 1].length || matrix[row][col] == 1) {
+        else if (row >= matrix.length || col >= matrix[row].length || matrix[row][col] == 1) {
             return 0;
         }
 
@@ -132,7 +117,61 @@ public class Solution {
         return dp[row][col];
     }
 
-    public static void main(String [] args) {
+    // [T]urn the Problem Around
+    public static int totalPaths(int [][] matrix) {
+        int [][] dp = new int [matrix.length + 1][matrix[0].length + 1];
 
+        for (int row = 0; row < dp.length; row++) {
+            for (int col = 0; col < dp[row].length; col++) {
+                if (row != 0 && col != 0) {
+                    if (row == 1 && col == 1) {
+                        dp[row][col] = 1;
+                    }
+                    else if (matrix[row - 1][col - 1] == 0) {
+                        dp[row][col] = dp[row - 1][col] + dp[row][col - 1];
+                    }
+                }
+            }
+        }
+
+        int result = totalPaths(matrix, 0, 0, new int [matrix.length][matrix[matrix.length - 1].length]);
+        assert (totalPaths(matrix, 0, 0) == result);
+        
+        System.out.println(result + " " + dp[matrix.length][matrix[matrix.length - 1].length]);
+        
+        assert (result == dp[matrix.length][matrix[matrix.length - 1].length]);
+
+        return result;
+    }
+
+    public static void main(String [] args) {
+        final int caseCount = 100;
+        Random random = new Random();
+
+        for (int caseIndex = 0; caseIndex < caseCount; caseIndex++) {
+            int [][] matrix = new int [random.nextInt(24) + 1][random.nextInt(24) + 1];
+            System.out.println("Case Number " + caseIndex);
+
+            for (int row = 0; row < matrix.length; row++) {
+                for (int col = 0; col < matrix[row].length; col++) {
+                    if (row == 0 && col == 0) {
+                        matrix[row][col] = 0;
+                    }
+                    else {
+                        if (random.nextInt(100) >= 85) {
+                            matrix[row][col] = 1;
+                        }
+                        else {
+                            matrix[row][col] = 0;
+                        }
+                    }
+                    System.out.print(matrix[row][col] + " ");
+                }
+                System.out.println("");
+            }
+
+            System.out.println("");
+            totalPaths(matrix);
+        }
     }
 }
