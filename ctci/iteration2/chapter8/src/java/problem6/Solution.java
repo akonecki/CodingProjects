0118cae96f23a4ce451ee48969ddb41204815537
awkonecki <awkonecki.developer@gmail.com>
2018-10-04 11:00:55 -0700

    [FTR] Chapter 8 Problem 6 - [A]nalysis Towers of Hanoi
    Feature Description
    
    Providing basic analysis of the problem in terms of dynamic programming
    capability in addition to the time and memory impact.
    
    Related Issues
    - #183

--- ctci/iteration2/chapter8/src/java/problem6/Solution.java
+++ ctci/iteration2/chapter8/src/java/problem6/Solution.java
@@ -47,6 +47,20 @@ public class Solution {
         return count;
     }
     
+    // [A]nalysis
+    // 1. Does the problem have optimal sub structure.
+    // Yes - since each iteration is self-contained.
+    // 2. Does the problem have reoccurring problems?
+    // No - in the current form the stack values are only ever visited once if 
+    // taking into account the stack contents.
+
+    // Memory impact O(N) - linear w.r.t the the value of N for the stack space
+    // in addition to the function call stack.
+
+    // Performance O(2^N) - the number of counts helps indicate the number of 
+    // times the number of Moves is called successfully in which operations 
+    // occur.
+
     public static void main(String [] args) {
         for (int i = 1; i < 32; i++) {
             assert (numberOfMoves(i) == (1 << i) - 1);
