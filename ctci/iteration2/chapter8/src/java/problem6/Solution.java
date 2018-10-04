4bf151dd835b361b34b97ba37d4874d958a095b4
awkonecki <awkonecki.developer@gmail.com>
2018-10-04 11:50:03 -0700

    [FTR] Chapter 8 Problem 6 - [S]ubproblem Identification Towers of Hanoi
    Feature Description
    
    Implemented a caching aspect for the towers of hanoi that focuses more on the
    argument values with only keeping track of the passed in arguments.  There
    might be some invalid source, aux stack values due to no always modifying the
    memory location but the passed in value.
    
    Expect that the source and destination values are correct however, along with
    total number of recursive calls.  Could enforce zero of source and aux by
    using a object reference that contains the count instead but is un-necessary.
    
    Related Issues
    - #183

--- ctci/iteration2/chapter8/src/java/problem6/Solution.java
+++ ctci/iteration2/chapter8/src/java/problem6/Solution.java
@@ -1,4 +1,5 @@
 import java.util.Stack;
+import java.util.HashMap;
 
 public class Solution {
     
@@ -11,15 +12,19 @@ public class Solution {
             stack.push(i);
         }
 
-        int count = numberOfMoves(N, stack, new Stack<Integer>(), destination);
+        int count = 0; //numberOfMoves(N, stack, new Stack<Integer>(), destination);
 
+        /*
         for (int num : destination) {
             System.out.print(num + " ");
         }
         System.out.println("");
 
         System.out.println(count);
-
+        */
+        count = numberOfMoves(N, N, 0, 0, new HashMap<Integer, HashMap<Integer, HashMap<Integer, HashMap<Integer, Integer>>>>());
+        System.out.println(count);
+        //assert (count == numberOfMoves(N, N, 0, 0));
         return count;
     }
 
@@ -61,9 +66,76 @@ public class Solution {
     // times the number of Moves is called successfully in which operations 
     // occur.
 
+    // [S]ubproblem Identification & Momeization
+    // Reduction in total number of calls performed by using the multi-index 
+    // hashmap scheme
+    private static int numberOfMoves(int N, int source, int aux, int destination, HashMap<Integer, HashMap<Integer, HashMap<Integer, HashMap<Integer, Integer>>>> dp) {
+        // N - 1 has to be moved to aux first
+        // once N - 1 has been move to aux then Nth disk can be moved to 
+        // destination.
+        if (N <= 0) {
+            return 0;
+        }
+        int count = 0;
+
+        if (dp.containsKey(N) && 
+            dp.get(N).containsKey(source) && 
+            dp.get(N).get(source).containsKey(aux) && 
+            dp.get(N).get(source).get(aux).containsKey(destination)) {
+            return dp.get(N).get(source).get(aux).get(destination).intValue();
+        }
+
+        System.out.println(N + " " + source + " " + aux + " " + destination);
+
+        // move top n - 1 disks using destination as aux
+        count = numberOfMoves(N - 1, source, destination, aux, dp);
+
+        // move top of source to destination
+        count++;
+        destination++;
+        source--;
+
+        // move top N - 1 from aux to destination
+        count += numberOfMoves(N - 1, aux, source, destination, dp);
+
+        HashMap<Integer, Integer> auxMap = null;
+        HashMap<Integer, HashMap<Integer, Integer>> sourceMap = null;
+        HashMap<Integer, HashMap<Integer, HashMap<Integer, Integer>>> nMap = dp.get(N);
+
+        if (nMap == null) {
+            nMap = new HashMap<Integer, HashMap<Integer, HashMap<Integer, Integer>>>();
+            sourceMap = new HashMap<Integer, HashMap<Integer, Integer>>();
+            auxMap = new HashMap<Integer, Integer>();
+        }
+        else {
+            sourceMap = dp.get(N).get(source);
+
+            if (sourceMap == null) {
+                sourceMap = new HashMap<Integer, HashMap<Integer, Integer>>();
+                auxMap = new HashMap<Integer, Integer>();
+            }
+            else {
+                auxMap = dp.get(N).get(source).get(aux);
+
+                if (auxMap == null) {
+                    auxMap = new HashMap<Integer, Integer>();
+                }
+            }  
+        }
+
+        auxMap.put(destination, count);
+        sourceMap.put(aux, auxMap);
+        nMap.put(source, sourceMap);
+        dp.put(N, nMap);
+        return count;
+    }
+
     public static void main(String [] args) {
+        /*
         for (int i = 1; i < 32; i++) {
             assert (numberOfMoves(i) == (1 << i) - 1);
         }
+        */
+        numberOfMoves(5);
     }
 }
