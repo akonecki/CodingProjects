7df4d5ddf7de9533b342d759c039bba7fc46191b
awkonecki <awkonecki.developer@gmail.com>
2018-10-05 13:38:04 -0700

    [FTR] Chapter 8 Problem 9 - [A]nalysis Parens
    Feature Description
    
    Rough analysis description of the possible memory and time impact.  This problem
    is a typical catalan number exercise.
    
    Related Issues
    - #183

--- ctci/iteration2/chapter8/src/java/problem9/Solution.java
+++ ctci/iteration2/chapter8/src/java/problem9/Solution.java
@@ -32,10 +32,28 @@ public class Solution {
         return;
     }
 
+    // [A]nalysis
+    // 1. does the problem have optimal substructure?
+    // yes each level of recursion is self contained.
+    // 2. does the problem have reoccurring sub problems?
+    // no due to the unique set of sb permutations that are being built.
+
+    // Performance
+    // Memory
+    // O(2*N * (2N)! / ((N+1)! * (N!)) + N)
+
+    // Time
+    // O(2^N)
+
     public static void main(String [] args) {
-        for (String string : generateParens(3)) {
+        for (int i = 1; i < 10; i++) {
+            System.out.println(i + " " + generateParens(i).size());
+        }
+        /*
+        for (String string : generateParens(4)) {
             System.out.println(string);
         }
         System.out.println("");
+        */
     }
 }
