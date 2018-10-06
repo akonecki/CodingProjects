1e0f5eb9e108d89a4fb5f423d29e260e91470e46
awkonecki <awkonecki.developer@gmail.com>
2018-10-05 18:50:41 -0700

    [FTR] Chapter 8 Problem 11 - [T]urn Around Coins
    Feature Description
    
    Serial dynammic programming to make the analysis a bit easier.  The
    iterative recursion adds a bit of difficulty in the case logic as to
    which values from the previous row should be used for calculation.
    
    Needed to account for current value - denomination value back inspection
    of the stack.
    
    Related Issues
    - #183

--- ctci/iteration2/chapter8/src/java/problem11/Solution.java
+++ ctci/iteration2/chapter8/src/java/problem11/Solution.java
@@ -6,18 +6,6 @@ public class Solution {
 
     // Assume change amount is in number of cents, as is the denominations.  
     // No half cents, only whole numbers.  Assumming no negative values either.
-    public static int numberOfWaysToMakeChange(int changeAmount, 
-        int [] denominations) 
-    {
-        if (changeAmount <= 0) {
-            return 0;
-        }
-
-        int ways = numberOfWaysToMakeChange(changeAmount, denominations, 0, new HashMap<Integer, HashMap<Integer, Integer>>());
-        assert (ways == numberOfWaysToMakeChange(changeAmount, denominations, 0));
-        return ways;
-    }
-
     private static int numberOfWaysToMakeChange(int valueRemaining, int [] denominations, int index) {
         if (valueRemaining == 0) {
             return 1;
@@ -31,9 +19,19 @@ public class Solution {
         int value = denominations[index];
         int count = 0;
 
+        /*
         while (count * value <= valueRemaining) {
             result += numberOfWaysToMakeChange(valueRemaining - (count * value), denominations, index + 1);
             count++;
+        }
+        */
+
+        // exclude
+        result += numberOfWaysToMakeChange(valueRemaining, denominations, index + 1);
+
+        // include
+        if (value <= valueRemaining) {
+            result += numberOfWaysToMakeChange(valueRemaining - value, denominations, index);
         }
 
         return result;
@@ -87,7 +85,57 @@ public class Solution {
         return result;
     }
 
+    // [T]urn Problem Around
+    public static int numberOfWaysToMakeChange(int changeAmount, 
+        int [] denominations) 
+    {
+        if (changeAmount <= 0) {
+            return 0;
+        }
+
+        int ways = numberOfWaysToMakeChange(changeAmount, denominations, 0, new HashMap<Integer, HashMap<Integer, Integer>>());
+        assert (ways == numberOfWaysToMakeChange(changeAmount, denominations, 0));
+
+        int [][] dp = new int [denominations.length + 1][changeAmount + 1];
+
+        for (int row = 0; row < dp.length; row++) {
+            for (int col = 0; col < dp[row].length; col++) {
+                if (row != 0 && col != 0) {
+                    // col is the denomination.
+                    // row is the amount
+                    // can only perform an add 
+                    
+                    // exclude 
+                    int value = dp[row - 1][col];
+
+                    // include 
+                    if (col >= denominations[row - 1] && col % denominations[row - 1] == 0) {
+                        // 
+
+                        if (denominations[row - 1] == col) {
+                            value += 1;
+                        } 
+                        else {
+                            value += dp[row][col - denominations[row - 1]]; 
+                        }
+                    }
+                    else if (col > denominations[row - 1]) {
+                        // Performing dp[row - 1][col] earlier means dont need to perform dp[row - 1][col - denominations[row - 1]]
+                        value += dp[row][col - denominations[row - 1]];
+                    }
+
+                    dp[row][col] = value;
+                }
+                System.out.print(dp[row][col] + " ");
+            }
+            System.out.println("");
+        }
+
+        System.out.println(dp[denominations.length][changeAmount]);
+        return ways;
+    }
+
     public static void main(String [] args) {
-        System.out.println(numberOfWaysToMakeChange(100, new int [] {25, 10, 5, 1}));
+        System.out.println(numberOfWaysToMakeChange(65, new int [] {25, 10, 5, 1}));
     }
 }
