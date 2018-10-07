efcffedf7c5fe62847ae1059255835016073137b
awkonecki <awkonecki.developer@gmail.com>
2018-10-06 18:35:03 -0700

    [FTR] Chapter 8 Problem 13 - [S]ubproblem Memoization Stack of Boxes
    Feature Description
    
    [S]ubproblem memoization by introducing caching in the problem.
    
    Related Issues
    - #183

--- ctci/iteration2/chapter8/src/java/problem13/Solution.java
+++ ctci/iteration2/chapter8/src/java/problem13/Solution.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Solution {
 
@@ -18,43 +19,30 @@ public class Solution {
         }
 
         public int compareTo(Box box) {
-            if (this.width < box.width && this.height < box.height && this.depth < box.depth) {
-                return 1;
-            }
-            else if (this.width > box.width && this.height > box.height && this.depth > box.depth) {
-                return -1;
-            }
-            else {
-                return 0;
-            }    
+            return this.height - box.height;    
         }
     }
 
     // [F]unctional
-    private static int maxHeight(Box [] boxes, int boxIndex, int maxHeight, int currentHeight, Box lastBox) {
+    private static int maxHeight(Box [] boxes, int boxIndex, Box lastBox) {
         if (boxIndex >= boxes.length) {
-            if (currentHeight > maxHeight) {
-                return currentHeight;
-            }
-            else {
-                return maxHeight;
-            }
+            return 0;
         }
         
         // not included
-        maxHeight = maxHeight(boxes, boxIndex + 1, maxHeight, currentHeight, lastBox);
-
+        int notIncludedHeight = maxHeight(boxes, boxIndex + 1, lastBox);
+        int includedHeight = 0;
         // try to include
         if (lastBox == null || boxes[boxIndex].compareTo(lastBox) > 0) {
-            maxHeight = maxHeight(boxes, boxIndex + 1, maxHeight, currentHeight + boxes[boxIndex].height, boxes[boxIndex]);
+            includedHeight = maxHeight(boxes, boxIndex + 1, boxes[boxIndex]) + boxes[boxIndex].height;
         }
 
-        return maxHeight;
+        return Math.max(notIncludedHeight, includedHeight);
     }
 
     private static class BoxOrder implements Comparator<Box> {
         public int compare(Box boxA, Box boxB) {
-            return boxA.compareTo(boxB);
+            return boxB.height - boxA.height;
         }
     }
 
@@ -71,6 +59,27 @@ public class Solution {
     // Time
     // O(2^N)
 
+    // [S]ubproblem & Memoization
+    private static int maxHeight(Box [] boxes, int boxIndex, Box lastBox, int [] stackMap) {
+        if (boxIndex >= boxes.length) {
+            return 0;
+        }
+        
+        // not included
+        int notIncludedHeight = maxHeight(boxes, boxIndex + 1, lastBox, stackMap);
+        int includedHieght = 0;
+        // try to include
+        if (lastBox == null || boxes[boxIndex].compareTo(lastBox) > 0) {
+            if (stackMap[boxIndex] == 0) {
+                stackMap[boxIndex] = maxHeight(boxes, boxIndex + 1, boxes[boxIndex], stackMap) + boxes[boxIndex].height;
+            }
+
+            includedHieght = stackMap[boxIndex];
+        }
+
+        return Math.max(notIncludedHeight, includedHieght);
+    }
+
     public static int maxHeight(int [][] boxes) {
         Box [] sortedBoxes = new Box [boxes.length];
 
@@ -80,11 +89,31 @@ public class Solution {
             index++;
         }
 
-        Arrays.sort(sortedBoxes, new BoxOrder());
-        return maxHeight(sortedBoxes, 0, 0, 0, null);
+        //Arrays.sort(sortedBoxes, new BoxOrder());
+        System.out.println(maxHeight(sortedBoxes, 0, null));
+        System.out.println(maxHeight(sortedBoxes, 0, null, new int [sortedBoxes.length]));
+        //assert (maxHeight(sortedBoxes, 0, null) == maxHeight(sortedBoxes, 0, null, new int [sortedBoxes.length]));
+
+        return 0;
     }
 
     public static void main(String [] args) {
+        final int numberOfCases = 100;
+        Random random = new Random();
+
+        for (int caseNumber = 0; caseNumber < numberOfCases; caseNumber++) {
+            int boxCount = random.nextInt(100) + 1;
+            int [][] boxes = new int [boxCount][3];
+
+            for (int [] box : boxes) {
+                box[0] = random.nextInt(20) + 1;
+                box[1] = random.nextInt(20) + 1;
+                box[2] = random.nextInt(20) + 1;
+            }
+
+            maxHeight(boxes);
+        }
+        
         System.out.println(maxHeight(new int [][] {
             {1,2,3},
             {2,3,4},
