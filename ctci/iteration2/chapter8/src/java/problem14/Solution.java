df08cc405b0055211aaf3934e1aa99562cf95633
awkonecki <awkonecki.developer@gmail.com>
2018-10-06 20:50:25 -0700

    [FTR] Chapter 8 Problem 14 - [S]ubproblem Memoization Boolean Evaluation
    Feature Description
    
    Added in memoization cached values that relies on the desired boolean string
    prepended of the evaluation string to create a key with the total count as the
    value.
    
    Related Issues
    - #183

--- ctci/iteration2/chapter8/src/java/problem14/Solution.java
+++ ctci/iteration2/chapter8/src/java/problem14/Solution.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 public class Solution {
     // [F]unctional
     private static int evalWays(String expression, boolean answerDesired) {
@@ -47,8 +49,52 @@ public class Solution {
     // Time
     // O((N/2)^N)
 
+    // [S]ubproblem Memoization
+    private static int evalWays(String expression, boolean answerDesired, HashMap<String, Integer> map) {
+        if (expression.length() == 0) {
+            return 0;
+        }
+        else if (expression.length() == 1) {
+            return (expression.charAt(0) == '1' ? true : false) == answerDesired ? 1 : 0;
+        }
+
+        if (map.containsKey(answerDesired + expression)) {
+            return map.get(answerDesired + expression);
+        }
+
+        int count = 0;
+        for (int index = 1; index < expression.length(); index++) {
+            String leftExpression = expression.substring(0, index);
+            String rightExpression = expression.substring(index + 1, expression.length());
+
+            int leftTrueCount = evalWays(leftExpression, true);
+            int leftFalseCount = evalWays(leftExpression, false);
+            int rightTrueCount = evalWays(rightExpression, true);
+            int rightFalseCount = evalWays(rightExpression, false);
+
+            int trueTotal = 0;
+            switch (expression.charAt(index)) {
+                case '^':
+                trueTotal = leftTrueCount * rightFalseCount + leftFalseCount * rightTrueCount;
+                break;
+                case '&':
+                trueTotal = leftTrueCount * rightTrueCount;
+                break;
+                case '|':
+                trueTotal = leftTrueCount * rightFalseCount + leftFalseCount * rightTrueCount + leftTrueCount * rightTrueCount;
+                break;
+            }
+
+            count += answerDesired ? trueTotal : ((leftTrueCount + leftFalseCount) * (rightTrueCount + rightFalseCount)) - trueTotal;
+        }
+        
+        map.put(answerDesired + expression, count);
+
+        return count;
+    }
+
     public static int numberOfWays(String expression, boolean answerDesired) {
-        System.out.println(evalWays(expression, answerDesired));
+        assert (evalWays(expression, answerDesired) == evalWays(expression, answerDesired, new HashMap<String, Integer>()));
         return evalWays(expression, answerDesired);
     }    
 
