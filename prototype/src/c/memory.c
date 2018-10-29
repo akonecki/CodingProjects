9c58460fcd880c0a1b27e7e5353ba7e259a1ab75
awkonecki <awkonecki.developer@gmail.com>
2018-10-29 10:05:08 -0700

    [FTR] Endianess Detection
    Feature Description
    
    Detect of system endianess with union set logic.  Remember that big is
    the highest byte of data comes in the lowest address while little is the
    lowest byte of data comes in the lowest address.
    
    Related Issues
    - #199

--- prototype/src/c/memory.c
+++ prototype/src/c/memory.c
@@ -1,6 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+typedef union AUNION {
+    int i;
+    char array [4];
+} aunion_t;
+
 void malloc_wrapper(const void ** ptr, const size_t numberOfBytes) {
     printf("malloc_wrapper arg ptr address %p\n", &ptr);
     printf("malloc_wrapper arg ptr contents address %p\n", ptr);
@@ -11,9 +16,18 @@ void malloc_wrapper(const void ** ptr, const size_t numberOfBytes) {
     printf("after malloc_wrapper ptr value address %p\n", *ptr);
 }
 
+int isBigEndian() {
+    aunion_t my;
+
+    my.i = 0x12345678;
+
+    printf("%p %p %p %p\n", &my.array[0], &my.array[1], &my.array[2], &my.array[3]);
+    printf("%02x %02x %02x %02x\n", my.array[0], my.array[1], my.array[2], my.array[3]);
+
+    return !my.array[0];
+}
+
 int main(int argc, char * argv[]) {
-    
-    
     int * intPtr = NULL;
 
     printf("intPtr is at address %p\n", &intPtr);
@@ -27,6 +41,8 @@ int main(int argc, char * argv[]) {
     printf("intPtr is pointing to %p\n", intPtr);
 
     free (intPtr);
+
+    printf("is big endian result : %08x\n", isBigEndian());
 
     return 0;
 }
